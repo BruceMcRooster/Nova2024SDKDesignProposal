40ea288d808517639da5f6cb0ccc2d16939f7a12
Evan <evan@brucemcrooster.dev>
2024-10-05 20:58:59 -0400

    🔧 Created Python library
    This is very much not necessary, but it allows examples to import files, dummy implementations/proposed ones to us real packages, and other things
    
    Signed-off-by: Evan <evan@brucemcrooster.dev>

---
+++ sdk/python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nova',
+    packages=find_packages(),
+    version='2024.0.0',
+    description='Enables access to AI tools for the Nova Hackathon',
+    author='ScottyLabs',
+)
